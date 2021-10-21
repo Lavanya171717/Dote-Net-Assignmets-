b14b182792c4bd47d1e3249fa85e19e888862515
Lavanya171717 <89248501+Lavanya171717@users.noreply.github.com>
2021-10-21 21:56:47 +0530

    Add files via upload

---
+++ EFAssignment1/EFAssignment1/Models/Userdatum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFAssignment1.Models
+{
+    class Userdatum
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
