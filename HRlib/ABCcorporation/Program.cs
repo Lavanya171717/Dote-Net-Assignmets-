356e4106b4c5be8126d2aeac95576b3b46b57cdb
Lavanya171717 <89248501+Lavanya171717@users.noreply.github.com>
2021-10-21 22:09:20 +0530

    Add files via upload

---
+++ HRlib/ABCcorporation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABCcorporation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HRlib/HRlib/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace HRLib
+{
+    public class Employee
+    {
+        #region Fields & Properties
+
+        private string empName;
+
+        public string EmpName
+        {
+            get { return empName; }
+            set { empName = value; }
+        }
+
+        private string address;
+
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        #endregion
+
+        #region Constructor
+
+        public Employee()
+        {
+            empName = "Lavanya";
+            address = "kakinada";
+        }
+
+        public Employee(string ename, string eaddr)
+        {
+            this.EmpName = ename;
+            this.Address = eaddr;
+        }
+        #endregion
+
+       
+
+
+    }
+}
